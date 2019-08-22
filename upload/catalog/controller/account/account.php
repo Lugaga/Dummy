ec70b0db40a260396510ef3732b7ce02004152de
Antony <amwaniki180@gmail.com>
2019-08-22 15:46:11 +0300

    edit password and address

--- upload/catalog/controller/account/account.php
+++ upload/catalog/controller/account/account.php
@@ -28,4 +28,11 @@ class ControllerAccountAccount extends Controller {
 			unset($this->session->data['success']);
 		} else {
 			$data['success'] = '';
-		} 
+        } 
+        
+        $data['edit'] = $this->url->link('account/edit', '', true);
+		$data['password'] = $this->url->link('account/password', '', true);
+		$data['address'] = $this->url->link('account/address', '', true);
+		
+		$data['credit_cards'] = array();
+		
