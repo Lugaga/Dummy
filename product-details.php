d5053af0f1a969143984c980ca0514b2291c0692
Francis Mukuha <mukuha58@gmail.com>
2019-08-22 16:41:20 +0300

    add insert to wishlist

--- product-details.php
+++ product-details.php
@@ -22,3 +22,13 @@ $pid=intval($_GET['pid']);
 if(isset($_GET['pid']) && $_GET['action']=="wishlist" ){
 	if(strlen($_SESSION['login'])==0)
     { 
+        header('location:login.php');
+    }
+    else
+    {
+    mysqli_query($con,"insert into wishlist(userId,productId) values('".$_SESSION['id']."','$pid')");
+    $_SESSION['id']."','$pid')");
+header('location:my-wishlist.php');
+
+}
+}
