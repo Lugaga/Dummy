38e717c06c595376989d1f57ae974846f2237a07
Antony <amwaniki180@gmail.com>
2019-08-22 15:40:12 +0300

    showing customer logged in

---
+++ upload/catalog/controller/account/account.php
@@ -0,0 +1,19 @@
+<?php
+class ControllerAccountAccount extends Controller {
+	public function index() {
+		if (!$this->customer->isLogged()) {
+			$this->session->data['redirect'] = $this->url->link('account/account', '', true);
+
+			$this->response->redirect($this->url->link('account/login', '', true));
+		}
+
+		$this->load->language('account/account');
+
+		$this->document->setTitle($this->language->get('heading_title'));
+
+		$data['breadcrumbs'] = array();
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_home'),
+			'href' => $this->url->link('common/home')
+		);
