238ce978f70717b3217359c0f7fed1225719c5e9
Antony <amwaniki180@gmail.com>
2019-08-23 11:06:50 +0300

    registration modifications

--- login.php
+++ login.php
@@ -54,9 +54,15 @@ exit();
 }
 }
 
+
 ?>
-<!-- Meta -->
-<meta charset="utf-8">
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+
+        <!-- Meta -->
+        <meta charset="utf-8">
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 		<meta name="description" content="">
