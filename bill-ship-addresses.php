d9fd657e1940fa6b73c55a3af6626cb4585af16d
Francis Mukuha <mukuha58@gmail.com>
2019-08-23 07:49:11 +0300

    add billing adress variables

--- bill-ship-addresses.php
+++ bill-ship-addresses.php
@@ -6,3 +6,14 @@ if(strlen($_SESSION['login'])==0)
     {   
 header('location:index.php');
 }
+else{
+	// code for billing address updation
+	if(isset($_POST['update']))
+	{
+		$baddress=$_POST['billingaddress'];
+		$bstate=$_POST['bilingstate'];
+		$bcity=$_POST['billingcity'];
+		$bpincode=$_POST['billingpincode'];
+		$query=mysqli_query($con,"update users set billingAddress='$baddress',billingState='$bstate',billingCity='$bcity',billingPincode='$bpincode' where id='".$_SESSION['id']."'");
+		if($query)
+		{
