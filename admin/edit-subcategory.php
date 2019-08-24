e307467e10f6f7bc8aa5390dd6f93ffd897dada6
Lugaga <maureezgaga@gmail.com>
2019-08-24 17:47:08 +0300

    include config.php to connect to DB and update subcategory  fields

--- admin/edit-subcategory.php
+++ admin/edit-subcategory.php
@@ -1,3 +1,25 @@
+
+<?php
+session_start();
+include('include/config.php');
+if(strlen($_SESSION['alogin'])==0)
+	{	
+header('location:index.php');
+}
+else{
+date_default_timezone_set('Africa/Nairobi');// change according timezone
+$currentTime = date( 'd-m-Y h:i:s A', time () );
+
+
+if(isset($_POST['submit']))
+{
+	$category=$_POST['category'];
+	$subcat=$_POST['subcategory'];
+	$id=intval($_GET['id']);
+$sql=mysqli_query($con,"update subcategory set categoryid='$category',subcategory='$subcat',updationDate='$currentTime' where id='$id'");
+$_SESSION['msg']="Sub-Category Updated !!";
+
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
