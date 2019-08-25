fd1663400463bc2939ccf05c9b4f0c417b283eb9
Francis Mukuha <mukuha58@gmail.com>
2019-08-25 21:52:20 +0300

    create date variables

--- admin/assets/js/form-calendar.js
+++ admin/assets/js/form-calendar.js
@@ -15,3 +15,55 @@ var Calendar = function () {
                 revertDuration: 50 //  original position after the drag
             });
         });
+        var date = new Date();
+        var d = date.getDate();
+        var m = date.getMonth();
+        var y = date.getFullYear();
+        var form = '';
+        var calendar = $('#calendar').fullCalendar({
+            buttonText: {
+                prev: '<i class="fa fa-chevron-left"></i>',
+                next: '<i class="fa fa-chevron-right"></i>'
+            },
+            header: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'month,agendaWeek,agendaDay'
+            },
+            events: [{
+                title: 'Meeting with Boss',
+                start: new Date(y, m, 1),
+                className: 'label-default'
+            }, {
+                title: 'Bootstrap Seminar',
+                start: new Date(y, m, d - 5),
+                end: new Date(y, m, d - 2),
+                className: 'label-teal'
+            }, {
+                title: 'Lunch with Nicole',
+                start: new Date(y, m, d - 3, 12, 0),
+                className: 'label-green',
+                allDay: false
+            }],
+            editable: true,
+            droppable: true, // this allows things to be dropped onto the calendar !!!
+            drop: function (date, allDay) { // this function is called when something is dropped
+                // retrieve the dropped element's stored Event Object
+                var originalEventObject = $(this).data('eventObject');
+                var $categoryClass = $(this).attr('data-class');
+                // we need to copy it, so that multiple events don't have a reference to the same object
+                var copiedEventObject = $.extend({}, originalEventObject);
+                // assign it the date that was reported
+                copiedEventObject.start = date;
+                copiedEventObject.allDay = allDay;
+                if ($categoryClass)
+                    copiedEventObject['className'] = [$categoryClass];
+                // render the event on the calendar
+                // the last `true` argument determines if the event "sticks" (http://arshaw.com/fullcalendar/docs/event_rendering/renderEvent/)
+                $('#calendar').fullCalendar('renderEvent', copiedEventObject, true);
+                // is the "remove after drop" checkbox checked?
+                if ($('#drop-remove').is(':checked')) {
+                    // if so, remove the element from the "Draggable Events" list
+                    $(this).remove();
+                }
+            },
