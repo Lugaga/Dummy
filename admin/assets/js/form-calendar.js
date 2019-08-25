0c7170a0551adaaf71ddce0b046b805acd2962b0
Francis Mukuha <mukuha58@gmail.com>
2019-08-25 21:51:20 +0300

    craete calender function

---
+++ admin/assets/js/form-calendar.js
@@ -0,0 +1,5 @@
+var Calendar = function () {
+    //function to initiate Full CAlendar
+    var runCalendar = function () {
+        var $modal = $('#event-management');
+        $('#event-categories div.event-category').each(function () {
