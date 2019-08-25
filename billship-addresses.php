14a60d228be82d9db200194d6be187fd08cee8e6
peter max <petermax2004@gmail.com>
2019-08-25 13:16:45 +0300

    Add the links for the various css fils

--- billship-addresses.php
+++ billship-addresses.php
@@ -34,4 +34,34 @@ echo "<script>alert('Billing Address has been updated');</script>";
 		{
 echo "<script>alert('Shipping Address has been updated');</script>";
 		}
-	}
+    }
+    
+    ?>
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <!-- Meta -->
+            <meta charset="utf-8">
+            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+            <meta name="description" content="">
+            <meta name="author" content="">
+            <meta name="keywords" content="MediaCenter, Template, eCommerce">
+            <meta name="robots" content="all">
+    
+            <title>My Account</title>
+    
+            <!-- Bootstrap Core CSS -->
+            <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+            
+            <!-- Customizable CSS -->
+            <link rel="stylesheet" href="assets/css/main.css">
+            <link rel="stylesheet" href="assets/css/green.css">
+            <link rel="stylesheet" href="assets/css/owl.carousel.css">
+            <link rel="stylesheet" href="assets/css/owl.transitions.css">
+            <!--<link rel="stylesheet" href="assets/css/owl.theme.css">-->
+            <link href="assets/css/lightbox.css" rel="stylesheet">
+            <link rel="stylesheet" href="assets/css/animate.min.css">
+            <link rel="stylesheet" href="assets/css/rateit.css">
+            <link rel="stylesheet" href="assets/css/bootstrap-select.min.css">
+    
