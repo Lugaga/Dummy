09a1a225460504dda50753b163594d4a7523c47e
Francis Mukuha <mukuha58@gmail.com>
2019-08-25 21:51:45 +0300

    add eventobject

--- admin/assets/js/form-calendar.js
+++ admin/assets/js/form-calendar.js
@@ -3,3 +3,15 @@ var Calendar = function () {
     var runCalendar = function () {
         var $modal = $('#event-management');
         $('#event-categories div.event-category').each(function () {
+            var eventObject = {
+                title: $.trim($(this).text()) // use the element's text as the event title
+            };
+            // store the Event Object in the DOM element so we can get to it later
+            $(this).data('eventObject', eventObject);
+            // make the event draggable using jQuery UI
+            $(this).draggable({
+                zIndex: 999,
+                revert: true, // will cause the event to go back to its
+                revertDuration: 50 //  original position after the drag
+            });
+        });
