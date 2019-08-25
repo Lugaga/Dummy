301503f5b58706fda60e47dfb3bc7f1f35f1c3bf
peter max <petermax2004@gmail.com>
2019-08-25 13:19:50 +0300

    Include the top-header.php,main-header.php and menu-bar.php files

--- billship-addresses.php
+++ billship-addresses.php
@@ -64,4 +64,21 @@ echo "<script>alert('Shipping Address has been updated');</script>";
             <link rel="stylesheet" href="assets/css/animate.min.css">
             <link rel="stylesheet" href="assets/css/rateit.css">
             <link rel="stylesheet" href="assets/css/bootstrap-select.min.css">
-    
+            <link rel="stylesheet" href="assets/css/config.css">
+
+            <link href="assets/css/green.css" rel="alternate stylesheet" title="Green color">
+            <link href="assets/css/blue.css" rel="alternate stylesheet" title="Blue color">
+            <link href="assets/css/red.css" rel="alternate stylesheet" title="Red color">
+            <link href="assets/css/orange.css" rel="alternate stylesheet" title="Orange color">
+            <link href="assets/css/dark-green.css" rel="alternate stylesheet" title="Darkgreen color">
+            <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+            <link href='http://fonts.googleapis.com/css?family=Roboto:300,400,500,700' rel='stylesheet' type='text/css'>
+            <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+	    </head>
+    <body class="cnt-home">
+<header class="header-style-1">
+<?php include('includes/top-header.php');?>
+<?php include('includes/main-header.php');?>
+<?php include('includes/menu-bar.php');?>
+</header>
