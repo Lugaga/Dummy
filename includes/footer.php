2709d75b9efe9efb8a6812653752965f026f14b1
Emma Sharon <emmasharon19@gmail.com>
2019-08-25 16:08:45 +0300

    create section fr contact information

---
+++ includes/footer.php
@@ -0,0 +1,33 @@
+<footer id="footer" class="footer color-bg">
+	  <div class="links-social inner-top-sm">
+        <div class="container">
+            <div class="row">
+            	<div class="col-xs-12 col-sm-6 col-md-4">
+            		 <!-- ============================================================= CONTACT INFO ============================================================= -->
+<div class="contact-info">
+    <div class="footer-logo">
+        <div class="logo">
+            <a href="index.php">
+                
+<h3>Shopping Portal</h3>
+            </a>
+        </div><!-- /.logo -->
+    
+    </div><!-- /.footer-logo -->
+
+     <div class="module-body m-t-20">
+        <p class="about-us"> Nam libero tempore, cum soluta nobis est ses  eligendi optio cumque cum soluta nobis est ses  eligendi optio cumque.</p>
+    
+        <div class="social-icons">
+            
+        <a href="http://facebook.com/transvelo" class='active'><i class="icon fa fa-facebook"></i></a>
+        <a href="#"><i class="icon fa fa-twitter"></i></a>
+        <a href="#"><i class="icon fa fa-linkedin"></i></a>
+        <a href="#"><i class="icon fa fa-rss"></i></a>
+        <a href="#"><i class="icon fa fa-pinterest"></i></a>
+
+        </div><!-- /.social-icons -->
+    </div><!-- /.module-body -->
+
+</div><!-- /.contact-info -->
+<!-- ============================================================= CONTACT INFO : END ============================================================= -->  
