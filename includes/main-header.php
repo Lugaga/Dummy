be276e846e59623c43f0c7df2450ae2fae19982e
Emma Sharon <emmasharon19@gmail.com>
2019-08-25 16:15:29 +0300

    create a serch bar and a logo area

--- includes/main-header.php
+++ includes/main-header.php
@@ -16,3 +16,27 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-xs-12 col-sm-12 col-md-3 logo-holder">
+                <!-- ============================================================= LOGO ============================================================= -->
+<div class="logo">
+	<a href="index.php">
+		
+		<h2>E-gikomba</h2>
+
+	</a>
+</div>		
+</div>
+<div class="col-xs-12 col-sm-12 col-md-6 top-search-holder">
+<div class="search-area">
+    <form name="search" method="post" action="search-result.php">
+        <div class="control-group">
+
+            <input class="search-field" placeholder="Search here..." name="product" required="required" />
+
+            <button class="search-button" type="submit" name="search"></button>    
+
+        </div>
+    </form>
+</div><!-- /.search-area -->
+<!-- ============================================================= SEARCH AREA : END ============================================================= -->				</div><!-- /.top-search-holder -->
+
+				
