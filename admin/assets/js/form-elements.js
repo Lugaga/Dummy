d9610064abe13bd93e039db7e34e745c4a380ed6
Francis Mukuha <mukuha58@gmail.com>
2019-08-25 21:55:22 +0300

    add form element function

---
+++ admin/assets/js/form-elements.js
@@ -0,0 +1,8 @@
+var FormElements = function () {
+    var runInputLimiter = function () {
+        $('.limited').inputlimiter({
+            remText: 'You only have %n character%s remaining...',
+            remFullText: 'Stop typing! You\'re not allowed any more characters!',
+            limitText: 'You\'re allowed to input %n character%s into this field.'
+        });
+    };
