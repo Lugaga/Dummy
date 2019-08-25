bb65b833579f3e80a666aa26b3d9bf5a63082859
Emma Sharon <emmasharon19@gmail.com>
2019-08-25 22:08:43 +0300

    include brand-slider and footer into the page

--- track-orders.php
+++ track-orders.php
@@ -37,18 +37,18 @@ include('includes/config.php');
 	</head>
     <body class="cnt-home">
 	
-    <header class="header-style-1">
-    
-        <!-- ============================================== TOP MENU ============================================== -->
-    <?php include('includes/top-header.php');?>
-    <!-- ============================================== TOP MENU : END ============================================== -->
-    <?php include('includes/main-header.php');?>
-        <!-- ============================================== NAVBAR ============================================== -->
-    <?php include('includes/menu-bar.php');?>
-    <!-- ============================================== NAVBAR : END ============================================== -->
-    
-    </header>
-    <!-- ============================================== HEADER : END ============================================== -->
+<header class="header-style-1">
+
+	<!-- ============================================== TOP MENU ============================================== -->
+<?php include('includes/top-header.php');?>
+<!-- ============================================== TOP MENU : END ============================================== -->
+<?php include('includes/main-header.php');?>
+	<!-- ============================================== NAVBAR ============================================== -->
+<?php include('includes/menu-bar.php');?>
+<!-- ============================================== NAVBAR : END ============================================== -->
+
+</header>
+<!-- ============================================== HEADER : END ============================================== -->
 <div class="breadcrumb">
 	<div class="container">
 		<div class="breadcrumb-inner">
@@ -81,4 +81,48 @@ include('includes/config.php');
 </div>			</div><!-- /.row -->
 		</div><!-- /.sigin-in-->
 		<!-- ============================================== BRANDS CAROUSEL ============================================== -->
-<div 
+<div >
+
+<?php echo include('includes/brands-slider.php');?>
+</div>
+</div>
+<?php include('includes/footer.php');?>
+	<script src="assets/js/jquery-1.11.1.min.js"></script>
+	
+	<script src="assets/js/bootstrap.min.js"></script>
+	
+	<script src="assets/js/bootstrap-hover-dropdown.min.js"></script>
+	<script src="assets/js/owl.carousel.min.js"></script>
+	
+	<script src="assets/js/echo.min.js"></script>
+	<script src="assets/js/jquery.easing-1.3.min.js"></script>
+	<script src="assets/js/bootstrap-slider.min.js"></script>
+    <script src="assets/js/jquery.rateit.min.js"></script>
+    <script type="text/javascript" src="assets/js/lightbox.min.js"></script>
+    <script src="assets/js/bootstrap-select.min.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+	<script src="assets/js/scripts.js"></script>
+
+	<!-- For demo purposes – can be removed on production -->
+	
+	<script src="switchstylesheet/switchstylesheet.js"></script>
+	
+	<script>
+		$(document).ready(function(){ 
+			$(".changecolor").switchstylesheet( { seperator:"color"} );
+			$('.show-theme-options').click(function(){
+				$(this).parent().toggleClass('open');
+				return false;
+			});
+		});
+
+		$(window).bind("load", function() {
+		   $('.show-theme-options').delay(2000).trigger('click');
+		});
+	</script>
+	<!-- For demo purposes – can be removed on production : End -->
+
+	
+
+</body>
+</html>
