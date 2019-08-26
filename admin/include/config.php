d29205163e9fd21a792bb0fbedf45766108f6514
Francis Mukuha <mukuha58@gmail.com>
2019-08-26 08:23:30 +0300

    check db connection

--- admin/include/config.php
+++ admin/include/config.php
@@ -4,5 +4,8 @@ define('DB_USER','root');
 define('DB_PASS' ,'');
 define('DB_NAME', 'egikosh');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
-
+if (mysqli_connect_errno())
+{
+ echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
 ?>
